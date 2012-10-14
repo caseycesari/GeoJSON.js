7239e633abce07eb073b0b8aaa6faeae4bf49054
Casey Thomas <cpthomas@gmail.com>
2012-10-13 20:02:45 -0400

    Implemented handling of any geometry type

--- geojson.js
+++ geojson.js
@@ -1,10 +1,11 @@
-exports.version = '0.0.1';
+exports.version = '0.0.4';
 
 exports.defaults = {};
 
 exports.parse = function(objects, params) {
   var geojson = baseObj();
   var conf = setConf(params, objects[0]);
+  getGeomAttrList(params);
 
   objects.forEach(function(item){
     var feature = buildFeature(item, conf);
@@ -15,11 +16,35 @@ exports.parse = function(objects, params) {
 };
 
 // Helper functions
+var geoms = [
+  'Point',
+  'MultiPoint',
+  'LineString',
+  'MultiLineString',
+  'Polygon',
+  'MultiPolygon'
+];
+
+var geomAttrs = [];
+
 function baseObj() {
   return {
     "type": "FeatureCollection",
     "features": []
   };
+}
+
+function getGeomAttrList(params) {
+  for(var param in params) {
+    if(params.hasOwnProperty(param)) {
+      if(geoms.indexOf(param) !== -1 && param !== 'Point') {
+        geomAttrs.push(params[param]);
+      } else if (param === 'Point') {
+        geomAttrs.push(params[param][0]);
+        geomAttrs.push(params[param][1]);
+      }
+    }
+  }
 }
 
 function getAttrList(params, item) {
@@ -28,7 +53,9 @@ function getAttrList(params, item) {
   } else if (params.exclude) {
     var attrs = [];
     for(var attr in item) {
-      if (item.hasOwnProperty(attr) && params.exclude.indexOf(attr) === -1) {
+      if (item.hasOwnProperty(attr) &&
+        params.exclude.indexOf(attr) === -1 &&
+        geomAttrs.indexOf(attr) === -1) {
         attrs.push(attr);
       }
     }
@@ -39,34 +66,67 @@ function getAttrList(params, item) {
 }
 
 function setConf(params, item) {
-  return {
-    x: params.point[1],
-    y: params.point[0],
-    attrs: getAttrList(params, item)
-  };
+  var conf = {};
+  conf.geom = {};
+
+  for(var param in params) {
+    if(params.hasOwnProperty(param) && geoms.indexOf(param) !== -1){
+      conf.geom[param] = params[param];
+    }
+  }
+
+  conf.attrs = getAttrList(params, item);
+
+  return conf;
 }
 
 function buildFeature(item, conf) {
-  var feature = {
-    "type": "Feature",
-    "geometry": {
-      "type": "Point",
-      "coordinates": [Number(item[conf.y]), Number(item[conf.x])]
-    },
-    "properties" : {}
-  };
+  var feature = { "type": "Feature" };
+
+  feature.geometry = buildGeom(item, conf);
+  feature.properties = buildProps(item, conf);
+
+  return feature;
+}
+
+function buildGeom(item, conf) {
+  var geom = {};
+
+  for(var attr in item) {
+    if(item.hasOwnProperty(attr) && geomAttrs.indexOf(attr) !== -1) {
+      if(attr === conf.geom.Point[0]) {
+        geom.type = "Point";
+        geom.coordinates = [item[conf.geom.Point[1]], item[conf.geom.Point[0]]];
+
+        return geom;
+      } else {
+        for(var gtype in conf.geom) {
+          if(conf.geom.hasOwnProperty(gtype) && attr === conf.geom[gtype]) {
+            geom.type = gtype;
+            geom.coordinates = item[conf.geom[gtype]];
+          }
+        }
+
+        return geom;
+      }
+    }
+  }
+}
+
+function buildProps(item, conf) {
+  var properties = {};
 
   if (conf.attrs !== 'all') {
     conf.attrs.forEach(function(attr) {
-      feature.properties[attr] = item[attr];
+      properties[attr] = item[attr];
     });
-  } else { // include or exclude not specified. Include all fields except geom/coords
+  } else { // include or exclude not specified. Include all fields except geometry fields
     for(var attr in item) {
-      if(item.hasOwnProperty(attr) && (attr !== conf.x && attr !==conf.y)) {
-        feature.properties[attr] = item[attr];
+      if(item.hasOwnProperty(attr) && (geomAttrs.indexOf(attr) === -1)) {
+        properties[attr] = item[attr];
       }
     }
   }
 
-  return feature;
+  return properties;
 }
