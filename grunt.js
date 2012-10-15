457301a57b8d8a195fac02d760cbb736be9d2a1f
Casey Thomas <cpthomas@gmail.com>
2012-10-15 12:57:20 -0400

    Added grunt as devDependence to lint and generate minified file

---
+++ grunt.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: '<json:package.json>',
+    meta: {
+      banner: '// <%= pkg.name %>.js - v<%= pkg.version %>\n' +
+        '// (c) 2012 Casey Thomas, MIT License'
+    },
+    lint: {
+      files: ['geojson.js', 'test/test.js']
+    },
+    min: {
+      dist: {
+        src: ['<banner>', 'geojson.js'],
+        dest: 'geojson.min.js'
+      }
+    }
+  });
+
+  grunt.registerTask('default', 'min');
+};
